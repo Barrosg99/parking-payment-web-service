3a42d7383a4eb60b4308daf38c1678e4d5fb9d73
Gabriel Barros <barros.fontes@unesp.br>
2024-11-24 19:41:31 -0300

    finish message queue

--- src/parking-payment/parking-payment.module.ts
+++ src/parking-payment/parking-payment.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ParkingPaymentResolver } from './parking-payment.resolver';
 import { ParkingPaymentService } from './parking-payment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+
+import {
+  ParkingPayment,
+  ParkingPaymentSchema,
+} from './models/parking-payment.model';
 
 @Module({
-  providers: [ParkingPaymentResolver, ParkingPaymentService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: ParkingPayment.name, schema: ParkingPaymentSchema },
+    ]),
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: 'amqp://guest:guest@localhost:5672',
+      connectionInitOptions: { wait: false },
+    }),
+  ],
+  providers: [ParkingPaymentResolver, ParkingPaymentService],
 })
 export class ParkingPaymentModule {}
--- src/parking-payment/parking-payment.service.ts
+++ src/parking-payment/parking-payment.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ParkingPayment } from './models/parking-payment.model';
+import { Model } from 'mongoose';
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 
 @Injectable()
-export class ParkingPaymentService {}
+export class ParkingPaymentService {
+  constructor(
+    @InjectModel(ParkingPayment.name)
+    private parkingPaymentModel: Model<ParkingPayment>,
+  ) {}
+
+  @RabbitSubscribe({
+    exchange: '',
+    routingKey: 'parking.payment.queue',
+    queue: 'parking.payment.queue',
+  })
+  public async processPayment(msg) {
+    const statuses = ['failed', 'paid'];
+    const randomIndex = Math.floor(Math.random() * statuses.length);
+    const randomStatus = statuses[randomIndex];
+
+    await this.parkingPaymentModel.create({ ...msg, status: randomStatus });
+  }
+}
